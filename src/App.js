54bd379d0a32650695ca937e7574bbf2de11dd51
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-22 10:35:53 +0200

    Basic app structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,42 @@
+import { Component } from "react";
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Unit converter</h1>
+        </div>
+
+        <div>
+          <h2>convert</h2>
+          <div>
+            <div>
+              <select></select>
+              <button></button>
+            </div>
+            <div>
+              <input></input>
+              <p></p>
+            </div>
+          </div>
+          <div>
+            <button></button>
+            <p></p>
+          </div>
+        </div>
+
+        <div>
+          <h3>saved</h3>
+          <div>
+            {
+
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
